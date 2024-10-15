837fef33a756f49152da8dd820279c9ccc5a714c
mlsanchez22 <macarenals92@hotmail.com>
2024-10-15 19:02:30 +0200

    Añadiendo directorio específico

---
+++ 09_drug_repository/09_1_genexpresion_gtex_tiss_ctrol/09_1_genexpresion_gtex_tiss_ctrol.cmd
@@ -0,0 +1,9 @@
+Rscript /mnt/lustre/scratch/CBRA/research/projects/tcga_mm/code/09_drug_repository/09_1_genexpresion_gtex_tiss_ctrol/09_1_genexpresion_gtex_tiss_ctrol.R Breast
+Rscript /mnt/lustre/scratch/CBRA/research/projects/tcga_mm/code/09_drug_repository/09_1_genexpresion_gtex_tiss_ctrol/09_1_genexpresion_gtex_tiss_ctrol.R Colon
+Rscript /mnt/lustre/scratch/CBRA/research/projects/tcga_mm/code/09_drug_repository/09_1_genexpresion_gtex_tiss_ctrol/09_1_genexpresion_gtex_tiss_ctrol.R Esophagus
+Rscript /mnt/lustre/scratch/CBRA/research/projects/tcga_mm/code/09_drug_repository/09_1_genexpresion_gtex_tiss_ctrol/09_1_genexpresion_gtex_tiss_ctrol.R Brain
+Rscript /mnt/lustre/scratch/CBRA/research/projects/tcga_mm/code/09_drug_repository/09_1_genexpresion_gtex_tiss_ctrol/09_1_genexpresion_gtex_tiss_ctrol.R Lung
+Rscript /mnt/lustre/scratch/CBRA/research/projects/tcga_mm/code/09_drug_repository/09_1_genexpresion_gtex_tiss_ctrol/09_1_genexpresion_gtex_tiss_ctrol.R Lung
+Rscript /mnt/lustre/scratch/CBRA/research/projects/tcga_mm/code/09_drug_repository/09_1_genexpresion_gtex_tiss_ctrol/09_1_genexpresion_gtex_tiss_ctrol.R Pancreas
+Rscript /mnt/lustre/scratch/CBRA/research/projects/tcga_mm/code/09_drug_repository/09_1_genexpresion_gtex_tiss_ctrol/09_1_genexpresion_gtex_tiss_ctrol.R Skin
+Rscript /mnt/lustre/scratch/CBRA/research/projects/tcga_mm/code/09_drug_repository/09_1_genexpresion_gtex_tiss_ctrol/09_1_genexpresion_gtex_tiss_ctrol.R Stomach
